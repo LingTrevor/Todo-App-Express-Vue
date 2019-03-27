58397eaad074abeb61ae5c814ceb1944b9a81c9d
Trevor <lingtrevor@gmail.com>
2019-03-27 10:55:52 +0200

    Add is-done class if todo is done

--- client/src/components/Todos.vue
+++ client/src/components/Todos.vue
@@ -1,7 +1,12 @@
 <template>
   <Container>
     <ul class="todos-list">
-      <li v-for="todo in allTodos" :key="todo._id" class="todo">
+      <li
+        v-for="todo in allTodos"
+        :key="todo._id"
+        class="todo"
+        :class="{ 'is-done': todo.isDone }"
+      >
         {{ todo.title }}
         <i @click="deleteTodo(todo._id)" class="fas fa-trash-alt"></i>
       </li>
@@ -53,12 +58,24 @@ export default {
       color: #fbecf0;
       // background-color: #d03a63;
       background-color: #922341;
+      box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.2);
       cursor: pointer;
     }
     i {
       margin-top: 3px;
       margin-left: 3px;
       float: right;
+    }
+  }
+
+  .is-done {
+    text-decoration: line-through;
+    background-color: transparent;
+    border: 1px dashed #922341;
+
+    &:hover {
+      background-color: #fbecf0;
+      color: #922341;
     }
   }
 }
